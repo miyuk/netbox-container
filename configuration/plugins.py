83dc7edf1c21b5189a93faccf65cb2ab6f90f4df
miyuk <enough7531@gmail.com>
2026-07-26 01:07:20 +0900

    add plugins

---
+++ configuration/plugins.py
@@ -0,0 +1,35 @@
+from os import environ
+from netbox_branching.utilities import DynamicSchemaDict
+PLUGINS = [
+    "netbox_custom_objects",
+    "netbox_security",
+    "netbox_topology_views",
+    "netbox_dns",
+    "netbox_branching",
+]
+
+PLUGINS_CONFIG = {
+    "netbox_branching": {
+        "exempt_models": [
+            "netbox_custom_objects.customobjecttype",
+            "netbox_custom_objects.customobjecttypefield",
+        ],
+    },
+}
+
+DATABASES = DynamicSchemaDict({
+    'default': {
+        'NAME': environ.get('DB_NAME', 'netbox'),
+        'USER': environ.get('DB_USER', ''),
+        'PASSWORD': environ.get('DB_PASSWORD', ''),
+        'HOST': environ.get('DB_HOST', 'localhost'),
+        'PORT': environ.get('DB_PORT', ''),
+        'OPTIONS': {'sslmode': environ.get('DB_SSLMODE', 'prefer')},
+        'CONN_MAX_AGE': int(environ.get('DB_CONN_MAX_AGE', '300')),
+        'DISABLE_SERVER_SIDE_CURSORS': environ.get('DB_DISABLE_SERVER_SIDE_CURSORS', 'False').lower() == 'true',
+    }
+})
+
+DATABASE_ROUTERS = [
+    'netbox_branching.database.BranchAwareRouter',
+]
